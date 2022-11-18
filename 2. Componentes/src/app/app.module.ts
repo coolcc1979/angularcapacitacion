070bf67c1cf5dcdd59ab71df7d7ce6532ffb9cf7
Jonathan Vargas <jonathan.vargas01@epn.edu.ec>
2022-11-18 13:30:13 -0500

    ejercicio 2

---
+++ 2. Componentes/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SliderbarComponent } from './sliderbar/sliderbar.component';
+import { LogicoComponent } from './logico/logico.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SliderbarComponent,
+    LogicoComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ 2. Componentes/src/app/logico/logico.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LogicoComponent } from './logico.component';
+
+describe('LogicoComponent', () => {
+  let component: LogicoComponent;
+  let fixture: ComponentFixture<LogicoComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LogicoComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LogicoComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
